629d5526cde782a36063f3cb0dece2ef84b39faa
Olga Sergeenkova <o.y.sergeenkova@gmail.com>
2025-08-29 12:16:55 +0300

    feat: add reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const numStr = n.toString();
+  const reversedStr = numStr.split('').reverse().join('');
+  const reversedNum = parseInt(reversedStr, 10);
+  if (n < 0) {
+    return reversedNum;
+  }
+  return reversedNum;
 };
